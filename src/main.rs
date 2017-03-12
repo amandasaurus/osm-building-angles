9622dfe842d34e884b44a4cf8c2ad48cd90e6806
Rory McCann <rory@technomancy.org>
2017-03-12 20:12:56 +0100

    More accurate printing

--- src/main.rs
+++ src/main.rs
@@ -140,7 +140,7 @@ fn write_results(zoom_grouping: u8, first_results: HashMap<(u32, u32, i16), usiz
     let mut output_fp = fs::File::create(filename).unwrap();
     output_fp.write(b"zoom,x,y,angle,count\n").unwrap();
     for this_zoom in (1..zoom_grouping+1).rev() {
-        println!("Calculating zoom {}", this_zoom);
+        println!("Writing results for zoom {}", this_zoom);
         let mut new_level = HashMap::new();
         for ((x, y, angle), count) in results {
             writeln!(output_fp, "{},{},{},{},{}", this_zoom, x, y, angle, count).unwrap();
